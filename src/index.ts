c4170c79486dbdaffe83522aa696a00c5fdefbd1
Dmitry Gulin <gulid@amazon.com>
2023-03-30 13:17:24 -0400

    change version discovery code

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { AppRunnerClient } from "@aws-sdk/client-apprunner";
 import { debug } from '@actions/core';
 import { getConfig } from "./action-configuration";
 import { createOrUpdateService, findExistingService, waitToStabilize } from "./action-helper-functions";
+import { version as package_version } from "../package.json";
 
 // GitHub action handler function
 export async function run(): Promise<void> {
@@ -44,7 +45,7 @@ export async function run(): Promise<void> {
 }
 
 if (require.main === module) {
-    info(`Amazon App Runner GitHub action version: ${process.env.npm_package_version ?? 'undefined'}`);
+    info(`Amazon App Runner GitHub action version: ${package_version ?? 'undefined'}`);
 
     run().then(() => {
         info('App Runner step - DONE!');
